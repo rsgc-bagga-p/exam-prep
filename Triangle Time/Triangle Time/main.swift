b8bc9a72a05f014cfa670a7c78193d02a11315dd
rsgc-bagga-p <pbagga@rsgc.on.ca>
2016-05-31 10:07:17 -0400

    Triangle Time Finished

--- Triangle Time/Triangle Time/main.swift
+++ Triangle Time/Triangle Time/main.swift
@@ -8,33 +8,49 @@
 
 import Foundation
 
-var angle1 : Int = 0
-var angle2 : Int = 0
-var angle3 : Int = 0
+var angle1 : Int = -1
+var angle2 : Int = -1
+var angle3 : Int = -1
 
-print("Angle 1?", terminator:"")
-
+repeat{
+print("Angle 1? ", terminator:"")
 if let input1 = readLine(stripNewline: true){
-    
-    angle1 = Int(input1)!
+    if let input1AsInteger = Int(input1){
+        
+            angle1 = input1AsInteger
+    }
 }
+} while angle1 < 0
 
-print("Angle 2?", terminator:"")
+repeat {
+print("Angle 2? ", terminator:"")
 
 if let input2 = readLine(stripNewline: true){
     
-    angle1 = Int(input2)!
-}
+    if let input2AsInteger = Int(input2){
+        
+        angle2 = input2AsInteger
+    }}
+} while angle2 < 0
 
-print("Angle 3?", terminator:"")
+repeat {
+print("Angle 3? ", terminator:"")
 
 if let input3 = readLine(stripNewline: true){
     
-    angle1 = Int(input3)!
+    if let input3AsInteger = Int(input3){
+        
+        angle3 = input3AsInteger
 }
+    }
+} while angle3 < 0
 
 
 if ((angle1 + angle2 + angle3) > 180){
+    
+    print("error")
+    
+} else if ((angle1 + angle2 + angle3) < 180) {
     
     print("error")
     
@@ -46,7 +62,7 @@ else if (angle1 == angle2 && angle2 == angle3){
     
 }
 
-else if (angle1 != angle2){
+else if (angle1 != angle2 && angle2 != angle3){
     
     print("Scalene")
     
